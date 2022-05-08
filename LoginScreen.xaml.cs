8a7d0c73d013274cea012a5a883a11352c621f78
DenisGreznev <den.greznev@yandex.ru>
2022-05-08 10:52:17 +0300

    budjet

---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIKitTutorials
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           
+        }
+
+        private void home_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            home.DragMove();
+        }
+
+        private void Border1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MainWindow dashboard = new MainWindow();
+            dashboard.Show();
+            this.Close();
+            dashboard.btnMenu.IsChecked = true;
+            dashboard.PagesNavigation.Navigate(new System.Uri("Pages/HomePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
